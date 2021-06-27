95516528e98e7ef3ba88a748113861493ec06319
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2021-06-27 22:50:18 +0300

    Toggle botton ON/OFF

--- app.js
+++ app.js
@@ -34,14 +34,73 @@ function onBall1Click() {
         ball1.style.padding = '';
     }
 }
+
+
+
+
+
+
 function onBall2Click() {
     var ball = document.querySelector('.ball2');
-    alert('Ball2');
+    if (ball2Size < 400){
+        ball2SizeStep = 50;
+    } else {
+        ball2SizeStep = -50
+    }
+    ball2Size = ball2Size + ball2SizeStep;
+
+
+   if (ball2Size == 250) {
+    ball2.style.backgroundColor = 'orange';
+    /*bonus: multiple box shadows with  1 black inset and 2 outer grey blurs so it will look like a comet: offset-x | offset-y | blur-radius | spread-radius | color */
+    ball2.style.boxShadow = 'inset 0 5px 15px 10px rgba(0,0,0,1), 10px 10px 8px darkgrey, 15px 25px 18px grey';
+    ball2.style.border = '15px solid grey';
+    ball2.style.fontSize = '2rem';
+    ball2.style.padding  = '10px';
+  }
+//    else {
+//     ball2.style.backgroundColor = 'yellow';
+//     ball2.style.boxShadow = '';
+//     ball2.style.border = '';
+//     ball2.style.fontSize = '';
+//     ball2.style.padding = '';
+//   }
+
+
+
+   
+   
+
+
+    if (ball2Size == 400) {
+        ball2.style.backgroundColor = 'red';
+    } else {
+        ball1.style.backgroundColor = 'yellow';
+    }
+     
+    
+    ball2.innerText = ball2Size;
+    ball2.style.width = ball2Size;
+    ball2.style.height = ball2Size;
+
+     
 }
+
 function onBall3Click() {
-    var ball = document.querySelector('.ball3');
-    alert('Ball3');
+    let ball = document.querySelector('.ball3');
+     
+
+    if (ball.innerText === 'OFF') {
+        ball.innerText = 'On'
+        ball.classList.add('active')
+    } else {
+        ball.innerText = 'OFF'
+        ball.classList.remove('active')
+    }
+
+   
 }
+
 function onBall4Click() {
     var ball = document.querySelector('.ball4');
     alert('Ball4');
