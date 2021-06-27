bae8f60f69d76c14cfe0cbf42134796003c22b94
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2021-06-27 19:09:01 +0300

    change css properties -backgroundcolor,boxshadow,border and font-size in app.js when ball size is 250

--- app.js
+++ app.js
@@ -10,15 +10,28 @@ function onBall1Click() {
         ball1Size = 100;
     }
 
+   
+
     ball1.innerText = ball1Size;
     ball1.style.width = ball1Size;
     ball1.style.height = ball1Size;
 
+
     if (ball1Size == 400) {
         ball1.style.backgroundColor = 'red';
+    } else if (ball1Size == 250) {
+        ball1.style.backgroundColor = 'orange';
+        /*bonus: multiple box shadows with  1 black inset and 2 outer grey blurs so it will look like a comet: offset-x | offset-y | blur-radius | spread-radius | color */
+        ball1.style.boxShadow = 'inset 0 5px 15px 10px rgba(0,0,0,1), 10px 10px 8px darkgrey, 15px 25px 18px grey';
+        ball1.style.border = '15px solid grey';
+        ball1.style.fontSize = '2rem';
+        ball1.style.padding  = '10px';
     } else {
         ball1.style.backgroundColor = 'yellow';
-
+        ball1.style.boxShadow = '';
+        ball1.style.border = '';
+        ball1.style.fontSize = '';
+        ball1.style.padding = '';
     }
 }
 function onBall2Click() {
@@ -33,3 +46,5 @@ function onBall4Click() {
     var ball = document.querySelector('.ball4');
     alert('Ball4');
 }
+
+
